c0d24a2162f6a0e9408ae2ecb6ba19ab73826cf8
Rafał Słota <roxeon@gmail.com>
2014-02-07 02:43:57 +0100

    VFS-361: update cached access times right away after file open

--- src/metaCache.cc
+++ src/metaCache.cc
@@ -60,7 +60,8 @@ bool MetaCache::getAttr(string path, struct stat* attr)
     if(attr != NULL) // NULL pointer is allowed to be used as parameter
         memcpy(attr, &(*it).second.second, sizeof(struct stat));
     
-    LOG(INFO) << "Return attr with times: " << attr->st_atime << " " << attr->st_mtime << " " << attr->st_ctime;
+    if(attr != NULL)
+        LOG(INFO) << "Return attr with times: " << attr->st_atime << " " << attr->st_mtime << " " << attr->st_ctime;
 
     return true;
 }
