bcc5701e3897cc1dca3642f5b796d2098b71de40
Rafał Słota <roxeon@gmail.com>
2014-02-09 21:46:49 +0100

    VFS-375: fix compile

--- test/include/testCommon.h
+++ test/include/testCommon.h
@@ -72,7 +72,7 @@ using namespace veil::client;
                             boost::shared_ptr<MetaCache>(new MetaCache()), \
                             boost::shared_ptr<StorageMapper>(new StorageMapper(boost::shared_ptr<FslogicProxy>(fslogic))), \
                             boost::shared_ptr<helpers::StorageHelperFactory>(new helpers::StorageHelperFactory()))); \
-        sleep(2);
+        sleep(5);
 
 #define COMMON_INTEGRATION_DEFS() \
         system::error_code ec; \
