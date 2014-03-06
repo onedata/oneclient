00ce396e8a89ead52a61e5bb2418188128094af3
Michał Sitko <msitko68@gmail.com>
2014-03-06 23:29:24 +0100

    [VFS-441] simple integration test for events

---
+++ test/integration_tests/events_test.erl
@@ -0,0 +1,41 @@
+%% ===================================================================
+%% @author Michal Sitko
+%% @copyright (C): 2013 ACK CYFRONET AGH
+%% This software is released under the MIT license 
+%% cited in 'LICENSE.txt'.
+%% @end
+%% ===================================================================
+%% @doc: PushChannelTest cluster side test driver.      
+%% @end
+%% ===================================================================
+-module(events_test).
+-include("test_common.hrl").
+
+-export([setup/1, teardown/2, exec/1]).
+
+
+setup(ccm) ->
+    ok;
+setup(worker) ->
+    test_common:register_user("peer.pem"),
+    ok.
+
+
+teardown(ccm, _State) ->
+    ok; 
+teardown(worker, _State) ->
+    ok.
+
+delete_file(FilePath) ->
+  dao_lib:apply(dao_vfs, remove_file, [FilePath], 1).
+
+exec({register_mkdir_handler, FilePath}) ->
+
+  EventHandler = fun({mkdir_event, AnsPid, UserId, _, _}) ->
+    %?info("Mkdir EventHandler"),
+    delete_file(FilePath)
+  end,
+
+  EventItem = {event_handler_item, standard, undefined, undefined, undefined, EventHandler, undefined},
+  %EventItem = #event_handler_item{processing_method = standard, handler_fun = EventHandler}, %, map_fun = EventHandlerMapFun, disp_map_fun = EventHandlerDispMapFun, config = ProcessingConfig},
+  gen_server:call({request_dispatcher, node()}, {rule_manager, 1, self(), {add_event_handler, {mkdir_event, EventItem}}}).
