16087bedc623e77f2c0e1dc2dff332d255546ed4
Michal Sitko <sitko68@gmail.com>
2014-04-23 13:32:37 +0200

    refactoring

--- src/IEventStream.cc
+++ src/IEventStream.cc
@@ -6,8 +6,6 @@
  */
 
 #include "events/IEventStream.h"
-#include "events/eventTransformer.h"
-#include "events/customActionStream.h"
 #include "fuse_messages.pb.h"
 
 using namespace veil::client::events;
@@ -50,54 +48,4 @@ shared_ptr<IEventStream> IEventStream::getWrappedStream() const
 void IEventStream::setWrappedStream(shared_ptr<IEventStream> wrappedStream)
 {
 	m_wrappedStream = wrappedStream;
-}
-
-EventTransformer::EventTransformer(vector<string> fieldNamesToReplace, vector<string> valuesToReplace, vector<string> newValues) :
-	m_fieldNamesToReplace(fieldNamesToReplace), m_valuesToReplace(valuesToReplace), m_newValues(newValues)
-{
-
-}
-
-shared_ptr<IEventStream> EventTransformer::fromConfig(const EventTransformerConfig & config)
-{
-	if(config.field_names_to_replace_size() != config.values_to_replace_size() || config.values_to_replace_size() != config.new_values_size()){
-		LOG(WARNING) << "Fields of EventTransformerConfig field_names_to_replace, values_to_replace and new_values are supposed to have the same length";
-		return shared_ptr<IEventStream>();
-	}
-	vector<string> fieldNamesToReplace;
-	for(int i=0; i<config.field_names_to_replace_size(); ++i){
-		fieldNamesToReplace.push_back(config.field_names_to_replace(i));
-	}
-	vector<string> valuesToReplace;
-	for(int i=0; i<config.values_to_replace_size(); ++i){
-		valuesToReplace.push_back(config.values_to_replace(i));
-	}
-	vector<string> newValues;
-	for(int i=0; i<config.new_values_size(); ++i){
-		newValues.push_back(config.new_values(i));
-	}
-	return shared_ptr<IEventStream> (new EventTransformer(fieldNamesToReplace, valuesToReplace, newValues));
-}
-
-shared_ptr<Event> EventTransformer::actualProcessEvent(shared_ptr<Event> event)
-{
-	shared_ptr<Event> newEvent (new Event(*event.get()));
-
-	// TODO: EventTransformer works only for string properties.
-	for(int i=0; i<m_fieldNamesToReplace.size(); ++i)
-	{
-		string fieldName = m_fieldNamesToReplace[i];
-		if(newEvent->getStringProperty(fieldName, "") == m_valuesToReplace[i]){
-			newEvent->m_stringProperties[fieldName] = m_newValues[i];
-		}
-	}
-	return newEvent;
-}
-
-CustomActionStream::CustomActionStream(shared_ptr<IEventStream> wrappedStream, boost::function<shared_ptr<Event>(boost::shared_ptr<Event>)> customActionFun) :
-	IEventStream(wrappedStream), m_customActionFun(customActionFun)
-{}
-
-shared_ptr<Event> CustomActionStream::actualProcessEvent(shared_ptr<Event> event){
-	return m_customActionFun(event);
 }
