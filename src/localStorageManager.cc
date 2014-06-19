6620707a329c47274f060274dfae6cc75ae9d203
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-06-19 07:02:26 +0200

    VFS-689 Catching boost::filesystem::filesystem_error exception.

--- src/localStorageManager.cc
+++ src/localStorageManager.cc
@@ -106,41 +106,49 @@ std::vector< std::pair<int, std::string> > LocalStorageManager::parseStorageInfo
     std::vector< std::pair<int, std::string> > storageInfo;
     path storageInfoPath = (mountPoint / STORAGE_INFO_FILENAME).normalize();
 
-    if(boost::filesystem::exists(storageInfoPath) && boost::filesystem::is_regular_file(storageInfoPath))
+    try
     {
-        boost::filesystem::ifstream storageInfoFile(storageInfoPath);
-        std::string line;
-
-        while(std::getline(storageInfoFile, line))
+        if(boost::filesystem::exists(storageInfoPath) && boost::filesystem::is_regular_file(storageInfoPath))
         {
-            std::vector<std::string> tokens;
-            boost::char_separator<char> sep(" ,{}");
-            boost::tokenizer< boost::char_separator<char> > tokenizer(line, sep);
+            boost::filesystem::ifstream storageInfoFile(storageInfoPath);
+            std::string line;
 
-            for(const auto &token: tokenizer) tokens.push_back(token);
-
-            // each line in file should by of form {storage id, relative path to storage against mount point}
-            if(tokens.size() == 2)
+            while(std::getline(storageInfoFile, line))
             {
-                try
-                {
-                    int storageId = boost::lexical_cast<int>(tokens[0]);
-                    path absoluteStoragePath = mountPoint;
+                std::vector<std::string> tokens;
+                boost::char_separator<char> sep(" ,{}");
+                boost::tokenizer< boost::char_separator<char> > tokenizer(line, sep);
 
-                    boost::algorithm::trim_left_if(tokens[1], boost::algorithm::is_any_of("./"));
-                    if(!tokens[1].empty())
-                    {
-                        absoluteStoragePath /= tokens[1];
-                    }
-                    storageInfo.push_back(std::make_pair(storageId, absoluteStoragePath.string()));
-                }
-                catch(boost::bad_lexical_cast const&)
+                for(const auto &token: tokenizer) tokens.push_back(token);
+
+                // each line in file should by of form {storage id, relative path to storage against mount point}
+                if(tokens.size() == 2)
                 {
-                    LOG(ERROR) << "Wrong format of storage id in file: " << storageInfoPath;
+                    try
+                    {
+                        int storageId = boost::lexical_cast<int>(tokens[0]);
+                        path absoluteStoragePath = mountPoint;
+
+                        boost::algorithm::trim_left_if(tokens[1], boost::algorithm::is_any_of("./"));
+                        if(!tokens[1].empty())
+                        {
+                            absoluteStoragePath /= tokens[1];
+                        }
+                        storageInfo.push_back(std::make_pair(storageId, absoluteStoragePath.string()));
+                    }
+                    catch(const boost::bad_lexical_cast &ex)
+                    {
+                        LOG(ERROR) << "Wrong format of storage id in file: " << storageInfoPath << ", error: " << ex.what();
+                    }
                 }
             }
         }
     }
+    catch (const boost::filesystem::filesystem_error &ex)
+    {
+        LOG(ERROR) << "Can not parse vfs_storage.info file: " << storageInfoPath << ", error: " << ex.what();
+    }
+
     return std::move(storageInfo);
 }
 
