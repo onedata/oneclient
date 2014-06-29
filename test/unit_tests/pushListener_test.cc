d3c1bf1dff65ea6a202e21dfb4510b618624bfa0
Konrad Zemek <konrad.zemek@gmail.com>
2014-06-29 12:04:42 +0200

    Fix pushListener test.

--- test/unit_tests/pushListener_test.cc
+++ test/unit_tests/pushListener_test.cc
@@ -9,9 +9,11 @@
 #include "jobScheduler_mock.h"
 #include "testCommon.h"
 #include "pushListener.h"
-#include <boost/thread.hpp>
-#include <boost/thread/thread_time.hpp>
 
+#include <atomic>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
 
 using namespace veil::protocol::communication_protocol;
 
@@ -23,95 +25,95 @@ class PushListenerTest
 : public ::testing::Test {
 protected:
     COMMON_DEFS();
-    
-    boost::mutex cbMutex;
-    boost::condition cbCond;
-    PushListener listener;
-    
-    int answerHandled;
-    
+
+    std::mutex cbMutex;
+    std::condition_variable cbCond;
+    std::unique_ptr<PushListener> listener;
+
+    std::atomic<int> answerHandled;
+
     virtual void SetUp() {
         COMMON_SETUP();
+        listener.reset(new PushListener());
         answerHandled = 0;
     }
-    
+
     virtual void TearDown() {
         COMMON_CLEANUP();
     }
-    
+
 public:
-    
+
     bool handler(const Answer &msg, bool ret = true)
     {
-        boost::unique_lock<boost::mutex> lock(cbMutex);
         if(msg.worker_answer() == "test")
-            answerHandled++;
+            ++answerHandled;
+
         cbCond.notify_all();
-        
+
         return ret;
     }
-    
+
 };
 
 
 TEST_F(PushListenerTest, simpleRegisterAndHandle)
 {
-    boost::unique_lock<boost::mutex> lock(cbMutex);
+    std::unique_lock<std::mutex> lock(cbMutex);
     Answer ans; // Message to handle
     ans.set_answer_status("ok");
     ans.set_worker_answer("test");
-    
-    listener.subscribe(boost::bind(&PushListenerTest::handler, this, _1, true));
-    
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
-    listener.onMessage(ans);
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
-    
-    ASSERT_EQ(4, answerHandled);
-    
+
+    listener->subscribe(std::bind(&PushListenerTest::handler, this, std::placeholders::_1, true));
+
+    listener->onMessage(ans);
+    ASSERT_TRUE(cbCond.wait_for(lock, std::chrono::seconds(10),
+                                [&]{ return answerHandled == 1; }));
+
+    listener->onMessage(ans);
+    ASSERT_TRUE(cbCond.wait_for(lock, std::chrono::seconds(10),
+                                [&]{ return answerHandled == 2; }));
+
+    listener->onMessage(ans);
+    listener->onMessage(ans);
+    ASSERT_TRUE(cbCond.wait_for(lock, std::chrono::seconds(10),
+                                [&]{ return answerHandled == 4; }));
 }
 
 
 TEST_F(PushListenerTest, removeHandler)
 {
-    boost::unique_lock<boost::mutex> lock(cbMutex);
+    std::unique_lock<std::mutex> lock(cbMutex);
     Answer ans; // Message to handle
     ans.set_answer_status("ok");
     ans.set_worker_answer("test");
-    
-    int handlerId = listener.subscribe(boost::bind(&PushListenerTest::handler, this, _1, false)); // Should be removed after first call
-    
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
-    listener.onMessage(ans);
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
-    
+
+    int handlerId = listener->subscribe(std::bind(&PushListenerTest::handler, this, std::placeholders::_1, false)); // Should be removed after first call
+
+    listener->onMessage(ans);
+    cbCond.wait_for(lock, std::chrono::milliseconds(500));
+
+    listener->onMessage(ans);
+    listener->onMessage(ans);
+    cbCond.wait_for(lock, std::chrono::milliseconds(500));
+
+
     ASSERT_EQ(1, answerHandled);
-    
+
     answerHandled = 0;
-    handlerId = listener.subscribe(boost::bind(&PushListenerTest::handler, this, _1, true));
-    
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
-    listener.unsubscribe(handlerId);
-    
-    listener.onMessage(ans);
-    listener.onMessage(ans);
-    cbCond.timed_wait(lock, posix_time::milliseconds(500));
-    
+    handlerId = listener->subscribe(std::bind(&PushListenerTest::handler, this, std::placeholders::_1, true));
+
+    listener->onMessage(ans);
+    cbCond.wait_for(lock, std::chrono::milliseconds(500));
+
+    listener->unsubscribe(handlerId);
+
+    listener->onMessage(ans);
+    listener->onMessage(ans);
+    cbCond.wait_for(lock, std::chrono::milliseconds(500));
+
     ASSERT_EQ(1, answerHandled);
-    
+
 }
 
 
