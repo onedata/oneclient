356da66a3e2302de252d1944d416142596098fe1
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-04-24 15:48:40 +0200

    Squashed 'helpers/' changes from 5228693..78e5435
    78e5435 VFS-1034 Apply recommended changes.
    ff38b2d Merge branch 'develop' of ssh://git.plgrid.pl:7999/vfs/helpers into feature/VFS-1034-oc-protocol-integration-and-tests
    caf0d71 Merge branch 'feature/VFS-1016-new-messages-handling-with-streaming' of ssh://git.plgrid.pl:7999/vfs/helpers into feature/VFS-1034-oc-protocol-integration-and-tests
    3d46673 VFS-1034 Add stream messages.
    dcf5c60 VFS-1034 Add end of stream message.
    e6f0758 VFS-1034 Add get protocol version message.
    8c787cd VFS-1034 Add protocol version message.
    2435249 Merge branch 'feature/VFS-1016-new-messages-handling-with-streaming_subtree' of ssh://git.plgrid.pl:7999/vfs/helpers into feature/VFS-1034-oc-protocol-integration-and-tests
    bb3247c VFS-1034 Add status message.
    
    git-subtree-dir: helpers
    git-subtree-split: 78e54351a00ab72fb82d24a12280812191e84850

---
+++ src/messages/status.cc
@@ -0,0 +1,113 @@
+/**
+ * @file status.cc
+ * @author Krzysztof Trzepla
+ * @copyright (C) 2015 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in
+ * 'LICENSE.txt'
+ */
+
+#include "messages/status.h"
+
+#include "client_messages.pb.h"
+#include "server_messages.pb.h"
+
+#include <boost/bimap.hpp>
+
+namespace {
+
+typedef boost::bimap<one::clproto::Status_Code, one::messages::Status::Code>
+    translation_type;
+
+translation_type createTranslation()
+{
+
+    translation_type translation;
+
+    translation.insert(translation_type::value_type(
+        one::clproto::Status_Code_VOK, one::messages::Status::Code::ok));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VENOENT,
+            one::messages::Status::Code::enoent));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VEACCES,
+            one::messages::Status::Code::eacces));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VEEXIST,
+            one::messages::Status::Code::eexist));
+    translation.insert(translation_type::value_type(
+        one::clproto::Status_Code_VEIO, one::messages::Status::Code::eio));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VENOTSUP,
+            one::messages::Status::Code::enotsup));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VENOTEMPTY,
+            one::messages::Status::Code::enotempty));
+    translation.insert(translation_type::value_type(
+        one::clproto::Status_Code_VEPERM, one::messages::Status::Code::eperm));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VEINVAL,
+            one::messages::Status::Code::einval));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VEDQUOT,
+            one::messages::Status::Code::edquot));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VENOATTR,
+            one::messages::Status::Code::enoattr));
+    translation.insert(translation_type::value_type(
+        one::clproto::Status_Code_VECOMM, one::messages::Status::Code::ecomm));
+    translation.insert(
+        translation_type::value_type(one::clproto::Status_Code_VEREMOTEIO,
+            one::messages::Status::Code::eremoteio));
+
+    return translation;
+}
+
+const translation_type translation = createTranslation();
+}
+
+namespace one {
+namespace messages {
+
+Status::Status(Code code)
+    : m_code{code}
+{
+}
+
+Status::Status(Code code, std::string description)
+    : m_code{code}
+    , m_description{std::move(description)}
+{
+}
+
+Status::Status(std::unique_ptr<ProtocolServerMessage> serverMessage)
+{
+    auto &statusMsg = serverMessage->status();
+
+    m_code = translation.left.at(statusMsg.code());
+
+    if (statusMsg.has_description())
+        m_description = statusMsg.description();
+}
+
+std::unique_ptr<ProtocolClientMessage> Status::serialize() const
+{
+    auto clientMsg = std::make_unique<ProtocolClientMessage>();
+    auto statusMsg = clientMsg->mutable_status();
+
+    statusMsg->set_code(translation.right.at(m_code));
+
+    if (m_description)
+        statusMsg->set_description(m_description.get());
+
+    return clientMsg;
+}
+
+Status::Code Status::code() const { return m_code; }
+
+const boost::optional<std::string> &Status::description() const
+{
+    return m_description;
+}
+
+} // namespace messages
+} // namespace one
