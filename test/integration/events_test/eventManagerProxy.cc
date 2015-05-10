a509eab0aed1580992812f3f0e7619a447aa9b73
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-05-10 12:39:43 +0200

    VFS-1040 Add integration tests for events.

---
+++ test/integration/events_test/eventManagerProxy.cc
@@ -0,0 +1,150 @@
+/**
+ * @file eventManagerProxy.cc
+ * @author Krzysztof Trzepla
+ * @copyright (C) 2015 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in
+ * 'LICENSE.txt'
+ */
+
+#include "context.h"
+#include "scheduler.h"
+#include "communication/communicator.h"
+#include "events/eventManager.h"
+#include "events/types/readEvent.h"
+#include "events/types/writeEvent.h"
+#include "events/types/truncateEvent.h"
+
+#include "messages.pb.h"
+
+#include <boost/python.hpp>
+#include <boost/make_shared.hpp>
+
+#include <atomic>
+#include <memory>
+
+using namespace one;
+using namespace one::client;
+using namespace one::client::events;
+using namespace one::communication;
+using namespace boost::python;
+
+class EventManagerProxy {
+public:
+    EventManagerProxy(const unsigned int connectionsNumber, std::string host,
+        const unsigned short port)
+    {
+        auto context = std::make_shared<Context>();
+        auto scheduler = std::make_shared<Scheduler>(1);
+        auto communicator = std::make_shared<Communicator>(
+            connectionsNumber, std::move(host), std::to_string(port), false);
+        communicator->connect();
+        context->setScheduler(std::move(scheduler));
+        context->setCommunicator(std::move(communicator));
+        m_eventManager = std::make_unique<EventManager>(std::move(context));
+    }
+
+    std::string emitReadEvent(std::string fileId, off_t offset, size_t size)
+    {
+        m_eventManager->emitReadEvent(fileId, offset, size);
+        auto event = ReadEvent{std::move(fileId), offset, size};
+        return setMessageStream(event.serialize())->SerializeAsString();
+    }
+
+    std::string emitWriteEvent(
+        std::string fileId, off_t offset, size_t size, off_t fileSize)
+    {
+        m_eventManager->emitWriteEvent(fileId, offset, size, fileSize);
+        auto event = WriteEvent{std::move(fileId), offset, size, fileSize};
+        return setMessageStream(event.serialize())->SerializeAsString();
+    }
+
+    std::string emitTruncateEvent(std::string fileId, off_t fileSize)
+    {
+        m_eventManager->emitTruncateEvent(fileId, fileSize);
+        auto event = TruncateEvent{std::move(fileId), fileSize};
+        return setMessageStream(event.serialize())->SerializeAsString();
+    }
+
+private:
+    std::unique_ptr<one::clproto::ClientMessage> setMessageStream(
+        std::unique_ptr<one::clproto::ClientMessage> clientMsg)
+    {
+        auto msgStream = clientMsg->mutable_message_stream();
+        msgStream->set_stream_id(0);
+        msgStream->set_sequence_number(m_sequenceNumber++);
+        return std::move(clientMsg);
+    }
+
+    std::atomic<uint64_t> m_sequenceNumber{0};
+    std::unique_ptr<EventManager> m_eventManager;
+};
+
+std::string prepareSerializedReadEventSubscription(uint64_t id,
+    size_t counterThreshold, uint64_t timeThreshold, size_t sizeThreshold)
+{
+    auto serverMsg = std::make_unique<one::clproto::ServerMessage>();
+    auto eventSubscriptionMsg = serverMsg->mutable_event_subscription();
+    auto readEventSubscriptionMsg =
+        eventSubscriptionMsg->mutable_read_event_subscription();
+    readEventSubscriptionMsg->set_id(id);
+    if (counterThreshold > 0)
+        readEventSubscriptionMsg->set_counter_threshold(counterThreshold);
+    if (timeThreshold > 0)
+        readEventSubscriptionMsg->set_time_threshold(timeThreshold);
+    if (sizeThreshold > 0)
+        readEventSubscriptionMsg->set_size_threshold(sizeThreshold);
+    return serverMsg->SerializeAsString();
+}
+
+std::string prepareSerializedWriteEventSubscription(uint64_t id,
+    size_t counterThreshold, uint64_t timeThreshold, size_t sizeThreshold)
+{
+    auto serverMsg = std::make_unique<one::clproto::ServerMessage>();
+    auto eventSubscriptionMsg = serverMsg->mutable_event_subscription();
+    auto writeEventSubscriptionMsg =
+        eventSubscriptionMsg->mutable_write_event_subscription();
+    writeEventSubscriptionMsg->set_id(id);
+    if (counterThreshold > 0)
+        writeEventSubscriptionMsg->set_counter_threshold(counterThreshold);
+    if (timeThreshold > 0)
+        writeEventSubscriptionMsg->set_time_threshold(timeThreshold);
+    if (sizeThreshold > 0)
+        writeEventSubscriptionMsg->set_size_threshold(sizeThreshold);
+    return serverMsg->SerializeAsString();
+}
+
+std::string prepareSerializedEventSubscriptionCancellation(uint64_t id)
+{
+    auto serverMsg = std::make_unique<one::clproto::ServerMessage>();
+    auto eventSubscriptionMsg = serverMsg->mutable_event_subscription();
+    auto eventSubscriptionCancellationMsg =
+        eventSubscriptionMsg->mutable_event_subscription_cancellation();
+    eventSubscriptionCancellationMsg->set_id(id);
+    return serverMsg->SerializeAsString();
+}
+
+namespace {
+boost::shared_ptr<EventManagerProxy> create(
+    const unsigned int connectionsNumber, std::string host,
+    const unsigned short port)
+{
+    return boost::make_shared<EventManagerProxy>(
+        connectionsNumber, std::move(host), port);
+}
+}
+
+BOOST_PYTHON_MODULE(events)
+{
+    class_<EventManagerProxy, boost::noncopyable>("EventManager", no_init)
+        .def("__init__", make_constructor(create))
+        .def("emitReadEvent", &EventManagerProxy::emitReadEvent)
+        .def("emitWriteEvent", &EventManagerProxy::emitWriteEvent)
+        .def("emitTruncateEvent", &EventManagerProxy::emitTruncateEvent);
+
+    def("prepareSerializedReadEventSubscription",
+        &prepareSerializedReadEventSubscription);
+    def("prepareSerializedWriteEventSubscription",
+        &prepareSerializedWriteEventSubscription);
+    def("prepareSerializedEventSubscriptionCancellation",
+        &prepareSerializedEventSubscriptionCancellation);
+}
--- test/integration/events_test/events_test.py
+++ test/integration/events_test/events_test.py
@@ -11,6 +11,8 @@ from test_common import *
 
 # noinspection PyUnresolvedReferences
 from environment import appmock, common, docker
+# noinspection PyUnresolvedReferences
+import events
 import appmock_client
 
 
@@ -31,7 +33,124 @@ class TestCommunicator:
     def teardown_class(cls):
         docker.remove(cls.result['docker_ids'], force=True, volumes=True)
 
-    def test_subscription(self):
-        time.sleep(0.5)
+    def test_subscription_counter_threshold(self):
+        manager = events.EventManager(1, self.ip, 5555)
 
-        assert 1 == 1
+        subscription = events.prepareSerializedReadEventSubscription(1, 1, 0,
+                                                                     0)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+        event = manager.emitReadEvent('fileId', 0, 10)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(1)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitReadEvent('fileId', 0, 10)
+        time.sleep(0.5)
+        assert 0 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+    def test_subscription_size_threshold(self):
+        manager = events.EventManager(1, self.ip, 5555)
+
+        subscription = events.prepareSerializedReadEventSubscription(1, 0, 0,
+                                                                     100)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+        event = manager.emitReadEvent('fileId', 0, 100)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(1)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitReadEvent('fileId', 0, 100)
+        time.sleep(0.5)
+        assert 0 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+    def test_subscription_time_threshold(self):
+        manager = events.EventManager(1, self.ip, 5555)
+
+        subscription = events.prepareSerializedReadEventSubscription(1, 0, 100,
+                                                                     0)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+        event = manager.emitReadEvent('fileId', 0, 1)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(1)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitReadEvent('fileId', 0, 1)
+        time.sleep(0.5)
+        assert 0 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+    def test_multiple_subscriptions(self):
+        manager = events.EventManager(1, self.ip, 5555)
+
+        subscription = events.prepareSerializedReadEventSubscription(1, 0, 0,
+                                                                     50)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+        subscription = events.prepareSerializedReadEventSubscription(2, 0, 0,
+                                                                     20)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+        subscription = events.prepareSerializedReadEventSubscription(3, 0, 0,
+                                                                     5)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+
+        event = manager.emitReadEvent('fileId', 0, 5)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(3)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitReadEvent('fileId', 0, 20)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(2)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitReadEvent('fileId', 0, 50)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(1)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitReadEvent('fileId', 0, 100)
+        time.sleep(0.5)
+        assert 0 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+
+    def test_different_subscriptions(self):
+        manager = events.EventManager(1, self.ip, 5555)
+
+        subscription = events.prepareSerializedReadEventSubscription(1, 1, 0,
+                                                                     0)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+        subscription = events.prepareSerializedWriteEventSubscription(2, 1, 0,
+                                                                      0)
+        appmock_client.tcp_server_send(self.ip, 5555, subscription)
+
+        event = manager.emitReadEvent('fileId', 1, 1)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(1)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+
+        event = manager.emitWriteEvent('fileId', 0, 1, 1)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+        event = manager.emitTruncateEvent('fileId', 0)
+        time.sleep(0.5)
+        assert 1 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
+        cancellation = events.prepareSerializedEventSubscriptionCancellation(2)
+        appmock_client.tcp_server_send(self.ip, 5555, cancellation)
+        event = manager.emitWriteEvent('fileId', 0, 1, 1)
+        time.sleep(0.5)
+        assert 0 == appmock_client.tcp_server_message_count(self.ip, 5555,
+                                                            event)
