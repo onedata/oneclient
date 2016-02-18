18d7abee1407a200a85913d9588c4202969a3bab
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2016-02-18 18:19:44 +0100

    Squashed 'bamboos/' changes from ccb41c3..a30c6f2
    a30c6f2 VFS-1621 Update pkg.py script.
    32df387 Merge branch 'develop' of ssh://git.plgrid.pl:7999/vfs/bamboos into feature/VFS-1621-oneclient-package-docker-plan-at
    f0fc80e Merge pull request #84 in VFS/bamboos from feature/VFS-1508-update-ember-gui-production-build to develop
    7f15c65 VFS-1508 fix missing import, improve error logging.
    d2765a0 Import sys in env.py
    c827505 VFS-1621 Create docker build reports.
    38243af Merge branch 'develop' of ssh://git.plgrid.pl:7999/vfs/bamboos into feature/VFS-1621-oneclient-package-docker-plan-at
    ca0f309 Merge pull request #79 in VFS/bamboos from feature/VFS-1508-update-ember-gui-production-build to develop
    2f522cc VFS-1508 fix a bug in bamboos
    8e869b3 VFS-1508 update make.py to use keys from .ssh/docker by default
    b627fe1 VFS-1508 update bamboos
    cbabf54 VFS-1508 merge with develop
    bb39552 VFS-1508 merge with develop
    4f0618b Merge commit '4eda32fe072e57b66a39dd5e24e35b9a701e8d41' into feature/VFS-1571-speed-up-compilation-through-caching
    829051f VFS-1508 merge with 1603
    6c37da5 Merge commit '34d0ca62a259c22ad1250790cd487b2123d4619d' into feature/VFS-1508-update-ember-gui-production-build
    ce6e993 Merge branch 'develop' of ssh://git.plgrid.pl:7999/vfs/bamboos into feature/VFS-1508-update-ember-gui-production-build
    2aa20fa VFS-1508 fix nvm sourcing during building
    5b2dca2 VFS-1520 - delete commented out line
    3fb1a6c VFS-1508 fix a minor bug in provider.up
    5f817ac VFS-1508 fix a minor bug in provider.up
    73b363f VFS-1508 direct output from ember live build to log file in dev environment
    2879d81 VFS-1520 - comment-out 'no-auto-compile ' opt from ct_run
    eaa29c7 VFS-1508 improve some specs and comments
    afdb76b VFS-1508 add missing storage options to env, improve env.up to fail if env configuration fails
    5547401 VFS-1508 revert some unneeded changes
    6bd6dce VFs-1508 merge with develop
    d7ccb09 VFS-1508 merge with develop
    38bfcb2 VFS-1508 introduce a switch in env.json to toggle gui livereload
    b6cc5f4 VFS-1508 slightly rework provider_up
    6e507dc VFS-1508 change owner of GUI tmp dir periodically rather than run ember build as user
    31c2b75 VFS-1508 run livereload as user
    5e9d8d1 VFS-1508 remove some unused code, improve some specs
    1f93f8b VFS-1508 change build process of GUI to use ember-cli, add livereload
    64bb564 Squashed 'helpers/' changes from ebab919..d64a745
    6ad2b80 Squashed 'helpers/' changes from 6c1d616..ebab919
    5b0c9df Squashed 'helpers/' changes from aa46c42..6c1d616
    ca5658f Squashed 'helpers/' changes from 56d8ba8..aa46c42
    
    git-subtree-dir: bamboos
    git-subtree-split: a30c6f25ef1ad5c4f58e4ec9e60e737b8bf3c8f2

--- docker/environment/env.py
+++ docker/environment/env.py
@@ -7,10 +7,11 @@ Brings up dockers with full onedata environment.
 """
 
 import os
+import sys
 import copy
-import subprocess
 import json
-import collections
+import sys
+import time
 from . import appmock, client, common, globalregistry, cluster_manager, \
     worker, provider_worker, cluster_worker, docker, dns
 
@@ -114,9 +115,10 @@ def up(config_path, image=default('image'), bin_am=default('bin_am'),
         print('')
         # Run env configurator with gathered args
         command = '''epmd -daemon
-        ./env_configurator.escript \'{0}\''''
+./env_configurator.escript \'{0}\'
+echo $?'''
         command = command.format(json.dumps(env_configurator_input))
-        docker.run(
+        docker_output = docker.run(
             image='onedata/builder',
             interactive=True,
             tty=True,
@@ -125,8 +127,22 @@ def up(config_path, image=default('image'), bin_am=default('bin_am'),
             name=common.format_hostname('env_configurator', uid),
             volumes=[(env_configurator_dir, '/root/build', 'ro')],
             dns_list=[dns_server],
-            command=command
+            command=command,
+            output=True
         )
+        # Result will contain output from env_configurator and result code in
+        # the last line
+        lines = docker_output.split('\n')
+        command_res_code = lines[-1]
+        command_output = '\n'.join(lines[:-1])
+        # print the output
+        print(command_output)
+        # check of env configuration succeeded
+        if command_res_code != '0':
+            # Let the command_output be flushed to console
+            time.sleep(2)
+            sys.exit(1)
+
 
     return output
 
