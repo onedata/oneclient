23c2fc29b62274105f3feaf4e9321f3914460a09
Michal Wrona <michal.wrona.4@gmail.com>
2016-03-09 15:06:01 +0100

    VFS-1652 Print to stdout instead of creating file in version script.

--- version.py
+++ version.py
@@ -1,31 +1,23 @@
 import re
-import sys
 from subprocess import check_output, CalledProcessError
 
 major, minor, patch, tweak, fallback = 0, 0, 0, '', 'unknown version'
 try:
     version = check_output(['git', 'describe', '--tags', '--always']).strip()
-    m = re.match('(?:(\d+)\.(\d+)\.(\d+)(?:-([\w-]+))?|(\w+))', version)
+    m = re.match('(?:(\d+)\.(\d+)\.(\d+)(?:-([\w\-]+))?|(\w+))', version)
     major, minor, patch, tweak, fallback = m.groups()
-    major = major if major else 0
-    minor = minor if minor else 0
-    patch = patch if patch else 0
-    tweak = tweak if tweak else ''
+    major = major or 0
+    minor = minor or 0
+    patch = patch or 0
+    tweak = tweak or ''
     fallback = 'git-{}'.format(fallback) if fallback else ''
 except (OSError, CalledProcessError):
     pass
 
-version_file = 'version.txt'
-if len(sys.argv) == 2:
-    version_file = '{}/{}'.format(sys.argv[1], version_file)
-
-cmake = '''
+print '''
 set(oneclient_VERSION_MAJOR {0})
 set(oneclient_VERSION_MINOR {1})
 set(oneclient_VERSION_PATCH {2})
 set(oneclient_VERSION_TWEAK {3})
 set(oneclient_VERSION_FALLBACK {4})
 '''.format(major, minor, patch, tweak, fallback)
-
-with open(version_file, 'w') as f:
-    f.write(cmake)
