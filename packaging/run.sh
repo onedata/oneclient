961c8177d133cea0e50246dc4a03bc26449901f3
Michal Wrona <michal.wrona.4@gmail.com>
2016-03-22 15:22:46 +0100

    VFS-1660 Merge scripts into one

--- packaging/run.sh
+++ packaging/run.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-/root/start_exports.sh
-oneclient -f ${@:1}
+# samba
+mkdir -p /run/samba
+smbd
+nmbd
+
+# nfs
+rpcbind
+rpc.nfsd
+exportfs -ar
+rpc.mountd
+rpc.nfsd
+rpc.statd
+
+oneclient -f $@
