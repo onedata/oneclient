0614d41043e810b76228e81c5c55ad1762674f91
Tomasz Lichon <xorver@gmail.com>
2016-04-27 11:40:44 +0200

    VFS-1747 Fix wrong usage of hashlib.

--- test/integration/fslogic_test/fslogic_test.py
+++ test/integration/fslogic_test/fslogic_test.py
@@ -94,7 +94,7 @@ def prepare_location_update_event(blocks, stream_id, sequence_number):
 
 
 def prepare_checksum(data):
-    md = hashlib.md4()
+    md = hashlib.new('MD4')
     md.update(data)
     hash = md.digest()
 
