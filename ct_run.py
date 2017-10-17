01801c68acdedb21fdb8c7d7ca5cd5524bf11828
Bartek Kryza <bkryza@gmail.com>
2017-10-17 22:54:36 +0200

    VFS-3763 Reverted builder to latest

--- ct_run.py
+++ ct_run.py
@@ -26,7 +26,7 @@ parser.add_argument(
 parser.add_argument(
     '--image', '-i',
     action='store',
-    default='docker.onedata.org/builder:VFS-3727',
+    default='onedata/builder',
     help='docker image to use as a test master',
     dest='image')
 
