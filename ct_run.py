715b642c75e7d9416ad6a85b67935fe30ee76094
Bartek Kryza <bkryza@gmail.com>
2020-07-03 16:32:17 +0200

    VFS-6474 Updated ct_run builder image

--- ct_run.py
+++ ct_run.py
@@ -26,7 +26,7 @@ parser.add_argument(
 parser.add_argument(
     '--image', '-i',
     action='store',
-    default='onedata/builder:2002-1',
+    default='onedata/builder:2002-2',
     help='docker image to use as a test master',
     dest='image')
 
