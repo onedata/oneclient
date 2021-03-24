3224dcca957cbaf48f860957cb5c8daa7e60359b
Bartek Kryza <bkryza@gmail.com>
2021-03-24 19:30:51 +0100

    VFS-7360 Refactored uuid util methods

---
+++ src/util/uuid.cc
@@ -0,0 +1,57 @@
+/**
+ * @file uuid.cc
+ * @author Bartek Kryza
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in
+ * 'LICENSE.txt'
+ */
+
+#include "uuid.h"
+
+#include "base64.h"
+
+#include <spdlog/fmt/fmt.h>
+
+namespace one {
+namespace client {
+namespace util {
+namespace uuid {
+
+folly::fbstring uuidToSpaceId(const folly::fbstring &uuid)
+{
+    folly::fbstring result;
+    folly::fbstring decodedUuid;
+    auto status = util::base64::base64_url_decode(uuid, decodedUuid);
+
+    if (status) {
+        std::vector<folly::StringPiece> v;
+        folly::split("#", decodedUuid, v);
+
+        if ((v[0] != "guid"))
+            throw std::invalid_argument("Invalid Onedata uuid format.");
+
+        auto spaceFragment = v[1];
+
+        if (spaceFragment.removePrefix("space_")) {
+            return spaceFragment.toString();
+        }
+
+        throw std::invalid_argument("Onedata uuid does not contain space id.");
+    }
+    else
+        throw std::invalid_argument("Base64 decoding of Onedata uuid failed.");
+}
+
+folly::fbstring spaceIdToSpaceUUID(const folly::fbstring &spaceId)
+{
+    folly::fbstring result;
+    util::base64::base64_url_encode(
+        fmt::format(
+            "guid#space_{}#{}", spaceId.toStdString(), spaceId.toStdString()),
+        result);
+    return result;
+}
+} // namespace uuid
+} // namespace util
+} // namespace client
+} // namespace one
