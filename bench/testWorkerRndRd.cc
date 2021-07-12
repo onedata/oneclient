9c1bda8a6df1161452539c3c9f14154cccd505f2
Bartek Kryza <bkryza@gmail.com>
2021-07-12 10:42:51 +0200

    VFS-7892 Refactored bench workers into cc files

---
+++ bench/testWorkerRndRd.cc
@@ -0,0 +1,82 @@
+/**
+ * @file testWorkerRndRd.cc
+ * @author Bartek Kryza
+ * @copyright (C) 2018 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in
+ * 'LICENSE.txt'
+ */
+
+#include "testWorkerRndRd.h"
+
+#include <folly/FBVector.h>
+#include <folly/Unit.h>
+
+namespace one {
+namespace bench {
+
+TestWorkerRndRd::TestWorkerRndRd(TestWorkerID id,
+    folly::IOThreadPoolExecutor &executor,
+    folly::futures::Barrier &startBarrier, folly::futures::Barrier &stopBarrier,
+    std::shared_ptr<one::helpers::StorageHelper> helper,
+    folly::MPMCQueue<TestResult, std::atomic, true> &resultsQueue,
+    const folly::fbvector<folly::fbstring> &fileIds, size_t fileSize,
+    size_t blockSize, bool blockAligned, int eventCount, int asyncBatchSize,
+    bool flush)
+    : TestWorker(id, executor, startBarrier, stopBarrier, std::move(helper),
+          resultsQueue, fileIds, fileSize, blockSize, blockAligned, eventCount,
+          asyncBatchSize, flush)
+{
+}
+
+void TestWorkerRndRd::operator()()
+{
+    auto fileCount = m_fileIds.size();
+
+    // Open the files
+    folly::fbvector<std::shared_ptr<one::helpers::FileHandle>> handles(
+        fileCount);
+
+    for (size_t i = 0; i < fileCount; i++) {
+        handles[i] = m_helper->open(m_fileIds[i], O_RDONLY, {}).get();
+    }
+
+    // Wait for all test workers to be ready
+    m_startBarrier.wait().get();
+
+    auto f = [this, id = m_id, fileCount, fileSize = m_fileSize,
+                 blockSize = m_blockSize, blockAligned = m_blockAligned,
+                 eventCount = m_eventCount, asyncBatchSize = m_asyncBatchSize,
+                 flush = m_flush, handles = std::move(handles)]() {
+        for (int k = 0; k < eventCount; k += asyncBatchSize) {
+
+            folly::fbvector<folly::Future<folly::Unit>> futs;
+            futs.reserve(asyncBatchSize);
+
+            for (int l = 0; l < asyncBatchSize; l++) {
+                auto fileIndex = std::rand() % fileCount;
+                auto offset =
+                    getRandomOffset(fileSize, blockSize, blockAligned);
+
+                auto start = Clock::now();
+
+                futs.emplace_back(
+                    handles[fileIndex]
+                        ->read(offset, blockSize)
+                        .then([this, start, blockSize, id, resultID = k + l](
+                                  folly::IOBufQueue &&buf) {
+                            postResult({id, resultID, start, Clock::now(), 1,
+                                buf.chainLength()});
+
+                            return folly::makeFuture();
+                        }));
+            }
+            folly::collectAll(futs.begin(), futs.end()).get();
+        }
+    };
+
+    f();
+
+    m_stopBarrier.wait().get();
+}
+} // namespace bench
+} // namespace one
--- bench/testWorkerRndWr.h
+++ bench/testWorkerRndWr.h
@@ -18,8 +18,6 @@
 #include <folly/Unit.h>
 #include <folly/executors/GlobalExecutor.h>
 
-#include <iostream>
-
 namespace one {
 namespace bench {
 
@@ -32,96 +30,10 @@ public:
         folly::MPMCQueue<TestResult, std::atomic, true> &resultsQueue,
         const folly::fbvector<folly::fbstring> &fileIds, size_t fileSize,
         size_t blockSize, bool blockAligned, int eventCount, int asyncBatchSize,
-        bool flush)
-        : TestWorker(id, executor, startBarrier, stopBarrier, std::move(helper),
-              resultsQueue, fileIds, fileSize, blockSize, blockAligned,
-              eventCount, asyncBatchSize, flush)
-    {
-    }
-
+        bool flush);
     virtual ~TestWorkerRndWr() = default;
 
-    virtual void operator()() override
-    {
-        auto fileCount = m_fileIds.size();
-
-        // Open the files
-        folly::fbvector<std::shared_ptr<one::helpers::FileHandle>> handles(
-            fileCount);
-
-        for (size_t i = 0; i < fileCount; i++) {
-            handles[i] = m_helper->open(m_fileIds[i], O_WRONLY, {}).get();
-        }
-
-        // Prepare random data for writing
-        auto data = randStr(m_blockSize);
-
-        // Wait for all test workers to be ready
-        m_startBarrier.wait().get();
-
-        auto f = [this, id = m_id, fileCount, fileSize = m_fileSize,
-                     blockSize = m_blockSize, blockAligned = m_blockAligned,
-                     eventCount = m_eventCount,
-                     asyncBatchSize = m_asyncBatchSize, flush = m_flush,
-                     data = std::move(data), handles = std::move(handles)]() {
-            for (int k = 0; k < eventCount; k += asyncBatchSize) {
-
-                folly::fbvector<folly::Future<folly::Unit>> futs;
-                futs.reserve(asyncBatchSize);
-
-                for (int l = 0; l < asyncBatchSize; l++) {
-                    auto fileIndex = std::rand() % fileCount;
-                    auto offset =
-                        getRandomOffset(fileSize, blockSize, blockAligned);
-
-                    auto storageBlockSize = m_helper->blockSize() == 0
-                        ? (1U << 31)
-                        : m_helper->blockSize();
-
-                    folly::IOBufQueue buf{
-                        folly::IOBufQueue::cacheChainLength()};
-                    buf.wrapBuffer(data.data(), data.size(), storageBlockSize);
-
-                    auto start = Clock::now();
-
-                    futs.emplace_back(
-                        handles[fileIndex]
-                            ->write(offset, std::move(buf), {})
-                            .then(folly::getIOExecutor().get(),
-                                [this, handle = handles[fileIndex], flush,
-                                    resultID = k + l](std::size_t written) {
-                                    if (flush)
-                                        return handle->flush();
-                                    else
-                                        return folly::makeFuture();
-                                })
-                            .then(folly::getIOExecutor().get(),
-                                [this, start, blockSize, id,
-                                    resultID = k + l]() {
-                                    postResult({id, resultID, start,
-                                        Clock::now(), 1, blockSize});
-
-                                    return folly::makeFuture();
-                                }));
-                }
-
-                folly::collectAll(futs.begin(), futs.end())
-                    .then([this, handles, fileSize](auto && /*f*/) {
-                        folly::fbvector<folly::Future<folly::Unit>> futs;
-                        for (auto &h : handles) {
-                            futs.emplace_back(
-                                m_helper->flushBuffer(h->fileId(), fileSize));
-                        }
-                        return folly::collectAll(futs.begin(), futs.end());
-                    })
-                    .get();
-            }
-        };
-
-        f();
-
-        m_stopBarrier.wait().get();
-    }
+    virtual void operator()() override;
 };
-}
-}
+} // namespace bench
+} // namespace one
