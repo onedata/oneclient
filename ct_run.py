7b95b9cc7ef4324aa23f35eb3fd558e86b50a879
Bartek Kryza <bkryza@gmail.com>
2021-10-26 13:00:02 +0200

    VFS-8483 Added NFS helper

--- ct_run.py
+++ ct_run.py
@@ -26,7 +26,7 @@ parser.add_argument(
 parser.add_argument(
     '--image', '-i',
     action='store',
-    default='onedata/builder:2102-5',
+    default='onedata/builder:2102-6',
     help='docker image to use as a test master',
     dest='image')
 
