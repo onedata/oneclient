1e8ac2695b48401a2909c01c59f201d4d679e026
Bartek Kryza <bkryza@gmail.com>
2022-02-22 13:27:05 +0100

    VFS-9054 Added CloseSession message implementation

---
+++ src/messages/closeSession.cc
@@ -0,0 +1,31 @@
+/**
+ * @file closeSession.cc
+ * @author Bartek Kryza
+ * @copyright (C) 2022 ACK CYFRONET AGH
+ * @copyright This software is released under the MIT license cited in
+ * 'LICENSE.txt'
+ */
+
+#include "closeSession.h"
+
+#include "messages.pb.h"
+
+
+namespace one {
+namespace messages {
+
+std::string CloseSession::toString() const
+{
+    return "type: 'CloseSession'";
+}
+
+std::unique_ptr<ProtocolClientMessage> CloseSession::serializeAndDestroy()
+{
+    auto clientMsg = std::make_unique<ProtocolClientMessage>();
+    clientMsg->mutable_get_configuration();
+
+    return clientMsg;
+}
+
+} // namespace messages
+} // namespace one
